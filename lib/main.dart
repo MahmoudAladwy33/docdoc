f1d3511ac7267b1e5caecca252eeda1b8529d7ec
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2024-09-27 01:58:11 +0300

    create app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:medilink/doc_app.dart';
 
 void main() {
-  runApp(const DocDoc());
-}
-
-class DocDoc extends StatelessWidget {
-  const DocDoc({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  runApp(const DocApp());
 }
